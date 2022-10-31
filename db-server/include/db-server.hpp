e285d95a5c427a7821dfd044310f0fae909e75cf
Hamdaan Khalid <khalid.hamdaan@gmail.com>
2022-10-30 19:13:41 -0700

    continue server work

--- db-server/include/db-server.hpp
+++ db-server/include/db-server.hpp
@@ -1,25 +1,45 @@
 #pragma once
 
+#include<iostream>
+#include<memory>
 #include<concurrent-hashmap.hpp>
+#include<logger.hpp>
 
 namespace DbServer {
-  
-
   class Db {
     public:
-    int Start(int port);
+
+    Db(int p);
+
     int Listen();
-    void Stop();
+
+    ~Db();
 
     private:
+
+    int port;
     int socketFd;
     int connection;
     unsigned long addrlen;
     sockaddr_in cliaddr;
     int listenDescriptor;
     bool listening;
-    ConcurMap::ConcurrentHashmap store;
+    CustomLogger::Logger& logger = CustomLogger::Logger::GetInstance(std::cout);
+
+    std::shared_ptr<ConcurMap::ConcurrentHashmap> store = std::make_shared<ConcurMap::ConcurrentHashmap>();
+
+    int start();
+    void stop();
 
     void handleBody(char* buf);
+
+    // handlers are pure function but write to buf
+    void getHandler();
+    void setHandler();
+    void delHandler();  
+
+    // buffer to json and json to buffer helpers
+    void vecBufToJsn();
+    void jsnVecBuf();
   };
 };
--- db-server/src/db-server.cpp
+++ db-server/src/db-server.cpp
@@ -2,49 +2,30 @@
 #include <sys/socket.h> // For socket functions
 #include <netinet/in.h> // For sockaddr_in
 #include <unistd.h> // For read
+#include <stdexcept>
+#include <string>
 
 #define SERVER_PORT 6969
 
-
-int DbServer::Db::Start(int port) {
-  int opt = 1;
-  socketFd = ::socket(AF_INET, SOCK_STREAM, 0);
-  if (-1 == socketFd) {
-    return -1;
+DbServer::Db::Db(int p): port(p) {
+  int success = start();
+  if (success != 0) {
+    logger.LogError("Unable to boot error");
+    throw "Unable to boot up server";
   }
-
-  // Forcefully attaching socket to the port 8080 
-  if (setsockopt(socketFd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt))) 
-  { 
-    return -1;
-  }
-
-  struct sockaddr_in sockaddr, cliaddr;
-  
-  sockaddr.sin_family = AF_INET;
-  sockaddr.sin_addr.s_addr = INADDR_ANY;
-
-  // htons is necessary to convert a number to network byte order
-  sockaddr.sin_port = htons(port);
-  
-  auto bindSuccess = ::bind(socketFd, (struct sockaddr*) &sockaddr, sizeof(sockaddr));
-
-  if (bindSuccess < 0) {
-    return -1;
-  }
-
-  listenDescriptor = listen(socketFd, 10);
-  if (listenDescriptor != 0)
-    return -1;
 }
 
 // blocking call
 int DbServer::Db::Listen() {
+  
   listening = true;
+
+  logger.LogInfo("Server listening and accepting connections on port " + std::to_string(port));
 
   while (listening) {
     connection = accept(socketFd, (struct sockaddr*) &cliaddr, (socklen_t*)&addrlen);
     if (connection < 0) {
+      logger.LogError("Error accepting connection");
       return -1;
     }
     
@@ -59,12 +40,59 @@ int DbServer::Db::Listen() {
   return 0;
 }
 
-void DbServer::Db::Stop() {
+DbServer::Db::~Db(){
+  stop();
+}
+
+int DbServer::Db::start() {
+  logger.LogInfo("Starting db server");
+  int opt = 1;
+  socketFd = ::socket(AF_INET, SOCK_STREAM, 0);
+  if (-1 == socketFd) {
+    logger.LogError("Server error");
+    return -1;
+  }
+
+  // Forcefully attaching socket to the port 8080 
+  if (setsockopt(socketFd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt))) 
+  { 
+    logger.LogError("Setsockopt error");
+    return -1;
+  }
+
+  struct sockaddr_in sockaddr, cliaddr;
+  
+  sockaddr.sin_family = AF_INET;
+  sockaddr.sin_addr.s_addr = INADDR_ANY;
+
+  // htons is necessary to convert a number to network byte order
+  sockaddr.sin_port = htons(port);
+  
+  auto bindSuccess = ::bind(socketFd, (struct sockaddr*) &sockaddr, sizeof(sockaddr));
+
+  if (bindSuccess < 0) {
+    logger.LogError("Unable to bind");
+    return -1;
+  }
+
+  listenDescriptor = listen(socketFd, 10);
+  if (listenDescriptor != 0) {
+    logger.LogError("Unable to listen");
+    return -1;
+  }
+  
+  logger.LogInfo("Server ready to start listening on port " + std::to_string(port));
+
+  return 0;
+}
+
+void DbServer::Db::stop() {
+  logger.LogInfo("Stopping db server");
   listening = false;
   close(listenDescriptor);
   shutdown(socketFd, SHUT_RDWR);
 }
 
 void DbServer::Db::handleBody(char* buf) {
-  
+
 }
