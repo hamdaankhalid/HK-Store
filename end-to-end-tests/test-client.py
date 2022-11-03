beaf939a9452f241a5a5c4c29ca0cb596b79b294
Hamdaan Khalid <khalid.hamdaan@gmail.com>
2022-11-02 20:54:30 -0700

    clean up tests

--- end-to-end-tests/test-client.py
+++ end-to-end-tests/test-client.py
@@ -2,13 +2,14 @@ import socket
 
 HOST = "127.0.0.1"  # The server's hostname or IP address
 PORT = 3000  # The port used by the server
+ENDIANESS = 'little'
 
 def set_request():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((HOST, PORT))
         
-        key_size = (3).to_bytes(4,'little')
-        val_size = (5).to_bytes(4,'little')
+        key_size = (3).to_bytes(4,ENDIANESS)
+        val_size = (5).to_bytes(4,ENDIANESS)
 
         request = b"SET " + key_size + b" " + val_size + b" cat spark"
 
@@ -23,7 +24,7 @@ def get_request():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((HOST, PORT))
         
-        key_size = (3).to_bytes(4,'little')
+        key_size = (3).to_bytes(4,ENDIANESS)
 
         request = b"GET " + key_size + b" " + b"cat"
 
@@ -38,7 +39,7 @@ def del_request():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((HOST, PORT))
         
-        key_size = (3).to_bytes(4,'little')
+        key_size = (3).to_bytes(4,ENDIANESS)
 
         request = b"DEL " + key_size + b" " + b"cat"
 
